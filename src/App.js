2122f9ebdf338cdf76490df09441a570bd4b9061
shivanirai08 <raishivani1406@gmail.com>
2025-06-11 09:34:30 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className='bg-zinc-50 p-5 border-2' >
+    </div>
+    </>
+  );
+}
+
+export default App;
